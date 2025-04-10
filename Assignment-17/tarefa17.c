94861e335878486d75864e92b43338126ec6dc5a
carlossb1 <josecarlosbarbt@gmail.com>
2025-04-10 16:10:22 -0300

    Task 17

---
+++ Assignment-17/tarefa17.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+int printer_function(void)
+{
+	printf("Hello, world!");
+
+		return 0;
+}
+
+
+
+int main (void)
+{
+	printf("Calling function ... \n");
+
+	printer_function();
+		
+		return 0;
+}
+
